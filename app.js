ceedaeb71373c8ec69210f12ec462c73514520c0
Sarah Wist <sarahwist107@gmail.com>
2020-05-19 16:07:55 -0700

    modified JavaScript into jQuery in js file and added button to html file

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const apiKey = "412552e87ada8cac1f10cba0d02c9993";
-const cityName = document.getElementById("input").value;
-console.log(city);
-function getInput() {
+
+// User clicks the search button
+// Then we grab the cityName from the searchCities input
+$("#button").on("click", function() {
+  const cityName = $("#searchCities").val();
+  console.log(cityName);
+  getInput(cityName);
+});
+
+function getInput(cityName) {
   const apiCurrent =
-    "api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}";
+    `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
   $.ajax({
     url: apiCurrent,
     method: "GET",
-  });
-
-  then(function (responseOneDay) {
+  }).then(function (responseOneDay) {
     $(".city").html("<h1>" + responseOneDay.name + " Weather Details</h1>");
     $(".date").html("<h2>" + responseOneDay.date + " Date</h2>");
     $(".wind").text("Wind Speed: " + responseOneDay.wind.speed);
@@ -21,96 +26,104 @@ function getInput() {
 
     $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
 
-    const uvIndex =
-      "api.openweathermap.org/data/2.5/uvi?q={apiKey}&lat={lat}&lon={lon}";
+    var lat = responseOneDay.coord.lat;
+    var lon = responseOneDay.coord.lon;
+
+    const uvIndexUrl =
+      `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
     $.ajax({
-      url: uvIndex,
+      url: uvIndexUrl,
       method: "GET",
-    });
+    }).then(function (responseUVIndex) {
+      $(".uvIndex").text("UV Index " + responseUVIndex.value);
+      console.log(typeof responseUVIndex.value);
 
-    then(function (responseUVIndex) {
-      $(".uv index").text("UV Index " + responseUVIndex.value);
-      console.log(responseUVIndex);
+      //const uviButton = $("<button>");
+      //uviButton.textContent = UVI;
 
-      const uviButton = document.createElement("button");
-      uvButton.textContent = UVI;
-
-      if (responseUVIndex.value > 0) {
-        uvButton.setAttribute("class", "bg-success rounded");
-      }
-      if (responseUVIndex.value > 4) {
-        uvButton.setAttribute("class", "bg-warning rounded");
-      }
-      if (responseUVIndex.value > 8) {
-        uvButton.setAttribute("class", "bg-danger rounded");
+      if (responseUVIndex.value > 0 && responseUVIndex.value <= 4) {
+        $(".uvIndex").attr("class", "bg-success rounded");
+      } else if (responseUVIndex.value > 4 && responseUVIndex.value <= 8) {
+        $(".uvIndex").attr("class", "bg-warning rounded");
+      } else if (responseUVIndex.value > 8) {
+        $(".uvIndex").attr("class", "bg-danger rounded");
       }
 
-      const apiFiveDay =
-        "api.openweathermap.org/data/2.5/forecast?q={cityName}&appid={apiKey}";
-      $.ajax({
-        url: apiFiveDay,
-        method: "GET",
-      });
-
-      then(function (responseFiveDay) {
-        console.log("5 day forecast response:");
-        console.log(responseFiveDay);
-
-        $(".day1Icon").text(
-          "Day 1 " + responseFiveDay.list[4].weather[0].icon
-        );
-        $(".day2Icon").text(
-          "Day 2 " + responseFiveDay.list[12].weather[0].icon
-        );
-        $(".day3Icon").text(
-          "Day 3 " + responseFiveDay.list[20].weather[0].icon
-        );
-        $(".day4Icon").text(
-          "Day 4 " + responseFiveDay.list[28].weather[0].icon
-        );
-        $(".day5Icon").text(
-          "Day 5 " + responseFiveDay.list[36].weather[0].icon
-        );
-
-        $(".day1temp").text("Day 1 " + responseFiveDay.list[4].main.temp);
-        $(".day2temp").text("Day 2 " + responseFiveDay.list[12].main.temp);
-        $(".day3temp").text("Day 3 " + responseFiveDay.list[20].main.temp);
-        $(".day4temp").text("Day 4 " + responseFiveDay.list[28].main.temp);
-        $(".day5temp").text("Day 5 " + responseFiveDay.list[36].main.temp);
-
-        $(".day1humidity").text(
-          "Day 1 " + responseFiveDay.list[4].main.humidity
-        );
-        $(".day2humidity").text(
-          "Day 2 " + responseFiveDay.list[12].main.humidity
-        );
-        $(".day3humidity").text(
-          "Day 3 " + responseFiveDay.list[20].main.humidity
-        );
-        $(".day4humidity").text(
-          "Day 4 " + responseFiveDay.list[28].main.humidity
-        );
-        $(".day5humidity").text(
-          "Day 5 " + responseFiveDay.list[36].main.humidity
-        );
-      });
     });
   });
 
-  $(".weatherDashboard").show();
+  const apiFiveDay =
+    `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
+  $.ajax({
+    url: apiFiveDay,
+    method: "GET",
+  }).then(function (responseFiveDay) {
+    console.log("5 day forecast response:");
+    console.log(responseFiveDay);
 
-  localStorage.setItem("previouscity", cityname);
-  if (citylist.includes(cityname)) {
-    return;
-  }
-  citylist.push(cityname);
-  const navItemOuterEl = $("<li class='nav-item'>");
-  navItemOuterEl.appendTo("#citylist");
-  $(
-    '<a class="nav-link active list-group-item bg-white text-dark border-light text-center" href="#">' +
-      cityname +
-      "</a>"
-  )
-    .css("textTransform", "capitalize")
-    .appendTo(navItemOuterEl);
+    $(".day1Icon").text(
+      "Day 1 " + responseFiveDay.list[4].weather[0].icon
+    );
+    $(".day2Icon").text(
+      "Day 2 " + responseFiveDay.list[12].weather[0].icon
+    );
+    $(".day3Icon").text(
+      "Day 3 " + responseFiveDay.list[20].weather[0].icon
+    );
+    $(".day4Icon").text(
+      "Day 4 " + responseFiveDay.list[28].weather[0].icon
+    );
+    $(".day5Icon").text(
+      "Day 5 " + responseFiveDay.list[36].weather[0].icon
+    );
+
+    $(".day1temp").text("Day 1 " + responseFiveDay.list[4].main.temp);
+    $(".day2temp").text("Day 2 " + responseFiveDay.list[12].main.temp);
+    $(".day3temp").text("Day 3 " + responseFiveDay.list[20].main.temp);
+    $(".day4temp").text("Day 4 " + responseFiveDay.list[28].main.temp);
+    $(".day5temp").text("Day 5 " + responseFiveDay.list[36].main.temp);
+
+    $(".day1humidity").text(
+      "Day 1 " + responseFiveDay.list[4].main.humidity
+    );
+    $(".day2humidity").text(
+      "Day 2 " + responseFiveDay.list[12].main.humidity
+    );
+    $(".day3humidity").text(
+      "Day 3 " + responseFiveDay.list[20].main.humidity
+    );
+    $(".day4humidity").text(
+      "Day 4 " + responseFiveDay.list[28].main.humidity
+    );
+    $(".day5humidity").text(
+      "Day 5 " + responseFiveDay.list[36].main.humidity
+    );
+  });
+
+  // local storage goes here
+
+  // $(".weatherDashboard").show();
+
+  // localStorage.setItem("previouscity", cityName);
+  // if (citylist.includes(cityname)) {
+  //   return;
+  // }
+  // citylist.push(cityname);
+  // const navItemOuterEl = $("<li class='nav-item'>");
+  // navItemOuterEl.appendTo("#citylist");
+  // $(
+  //   '<a class="nav-link active list-group-item bg-white text-dark border-light text-center" href="#">' +
+  //     cityname +
+  //     "</a>"
+  // )
+  //   .css("textTransform", "capitalize")
+  //   .appendTo(navItemOuterEl);
 }
+
+
+
+function example(x, y, z) {
+  console.log(x + y + z);
+}
+
+example(2, 3, 4);
