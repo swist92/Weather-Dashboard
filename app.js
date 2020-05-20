4948c873058aff75576e18d45e9ca156f0a51cd0
Sarah Wist <sarahwist107@gmail.com>
2020-05-19 23:43:19 -0700

    Restructured HTML and made changes to UV Index in JS

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const apiKey = "412552e87ada8cac1f10cba0d02c9993";
 
 // User clicks the search button
 // Then we grab the cityName from the searchCities input
-$("#button").on("click", function () {
+$("#button").on("click", function() {
   const cityName = $("#searchCities").val();
   console.log(cityName);
   getInput(cityName);
 });
 
 function getInput(cityName) {
-  const apiCurrent = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+  const apiCurrent =
+    `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
   $.ajax({
     url: apiCurrent,
     method: "GET",
@@ -28,7 +29,8 @@ function getInput(cityName) {
     var lat = responseOneDay.coord.lat;
     var lon = responseOneDay.coord.lon;
 
-    const uvIndexUrl = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
+    const uvIndexUrl =
+      `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
     $.ajax({
       url: uvIndexUrl,
       method: "GET",
@@ -36,20 +38,24 @@ function getInput(cityName) {
       $(".uvIndex").text("UV Index " + responseUVIndex.value);
       console.log(typeof responseUVIndex.value);
 
+      const tempF = (responseOneDay.main.temp - 273.15) * 1.8 + 32;
+
       //const uviButton = $("<button>");
       //uviButton.textContent = UVI;
 
       if (responseUVIndex.value > 0 && responseUVIndex.value <= 4) {
-        $(".uvIndex").attr("class", "bg-success rounded");
+        $(".uvIndex").attr("class", "bg-success pl-3 rounded");
       } else if (responseUVIndex.value > 4 && responseUVIndex.value <= 8) {
-        $(".uvIndex").attr("class", "bg-warning rounded");
+        $(".uvIndex").attr("class", "bg-warning pl-3 rounded");
       } else if (responseUVIndex.value > 8) {
-        $(".uvIndex").attr("class", "bg-danger rounded");
+        $(".uvIndex").attr("class", "bg-danger pl-3 rounded");
       }
+
     });
   });
 
-  const apiFiveDay = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
+  const apiFiveDay =
+    `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
   $.ajax({
     url: apiFiveDay,
     method: "GET",
@@ -57,11 +63,21 @@ function getInput(cityName) {
     console.log("5 day forecast response:");
     console.log(responseFiveDay);
 
-    $(".day1icon").text("Day 1 " + responseFiveDay.list[4].weather[0].icon);
-    $(".day2icon").text("Day 2 " + responseFiveDay.list[12].weather[0].icon);
-    $(".day3icon").text("Day 3 " + responseFiveDay.list[20].weather[0].icon);
-    $(".day4icon").text("Day 4 " + responseFiveDay.list[28].weather[0].icon);
-    $(".day5icon").text("Day 5 " + responseFiveDay.list[36].weather[0].icon);
+    $(".day1icon").text(
+      "Day 1 " + responseFiveDay.list[4].weather[0].icon
+    );
+    $(".day2icon").text(
+      "Day 2 " + responseFiveDay.list[12].weather[0].icon
+    );
+    $(".day3icon").text(
+      "Day 3 " + responseFiveDay.list[20].weather[0].icon
+    );
+    $(".day4icon").text(
+      "Day 4 " + responseFiveDay.list[28].weather[0].icon
+    );
+    $(".day5icon").text(
+      "Day 5 " + responseFiveDay.list[36].weather[0].icon
+    );
 
     $(".day1temp").text("Day 1 " + responseFiveDay.list[4].main.temp);
     $(".day2temp").text("Day 2 " + responseFiveDay.list[12].main.temp);
@@ -69,11 +85,21 @@ function getInput(cityName) {
     $(".day4temp").text("Day 4 " + responseFiveDay.list[28].main.temp);
     $(".day5temp").text("Day 5 " + responseFiveDay.list[36].main.temp);
 
-    $(".day1humidity").text("Day 1 " + responseFiveDay.list[4].main.humidity);
-    $(".day2humidity").text("Day 2 " + responseFiveDay.list[12].main.humidity);
-    $(".day3humidity").text("Day 3 " + responseFiveDay.list[20].main.humidity);
-    $(".day4humidity").text("Day 4 " + responseFiveDay.list[28].main.humidity);
-    $(".day5humidity").text("Day 5 " + responseFiveDay.list[36].main.humidity);
+    $(".day1humidity").text(
+      "Day 1 " + responseFiveDay.list[4].main.humidity
+    );
+    $(".day2humidity").text(
+      "Day 2 " + responseFiveDay.list[12].main.humidity
+    );
+    $(".day3humidity").text(
+      "Day 3 " + responseFiveDay.list[20].main.humidity
+    );
+    $(".day4humidity").text(
+      "Day 4 " + responseFiveDay.list[28].main.humidity
+    );
+    $(".day5humidity").text(
+      "Day 5 " + responseFiveDay.list[36].main.humidity
+    );
   });
 
   // local storage goes here
