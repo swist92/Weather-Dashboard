f96a234b81499b7900b2421aa6f0be061a0aeec9
Sarah Wist <sarahwist107@gmail.com>
2020-05-19 21:24:28 -0700

    Styled CSS sheet and modified html divs

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const apiKey = "412552e87ada8cac1f10cba0d02c9993";
 
 // User clicks the search button
 // Then we grab the cityName from the searchCities input
-$("#button").on("click", function() {
+$("#button").on("click", function () {
   const cityName = $("#searchCities").val();
   console.log(cityName);
   getInput(cityName);
 });
 
 function getInput(cityName) {
-  const apiCurrent =
-    `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+  const apiCurrent = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
   $.ajax({
     url: apiCurrent,
     method: "GET",
@@ -29,8 +28,7 @@ function getInput(cityName) {
     var lat = responseOneDay.coord.lat;
     var lon = responseOneDay.coord.lon;
 
-    const uvIndexUrl =
-      `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
+    const uvIndexUrl = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
     $.ajax({
       url: uvIndexUrl,
       method: "GET",
@@ -48,12 +46,10 @@ function getInput(cityName) {
       } else if (responseUVIndex.value > 8) {
         $(".uvIndex").attr("class", "bg-danger rounded");
       }
-
     });
   });
 
-  const apiFiveDay =
-    `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
+  const apiFiveDay = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
   $.ajax({
     url: apiFiveDay,
     method: "GET",
@@ -61,21 +57,11 @@ function getInput(cityName) {
     console.log("5 day forecast response:");
     console.log(responseFiveDay);
 
-    $(".day1Icon").text(
-      "Day 1 " + responseFiveDay.list[4].weather[0].icon
-    );
-    $(".day2Icon").text(
-      "Day 2 " + responseFiveDay.list[12].weather[0].icon
-    );
-    $(".day3Icon").text(
-      "Day 3 " + responseFiveDay.list[20].weather[0].icon
-    );
-    $(".day4Icon").text(
-      "Day 4 " + responseFiveDay.list[28].weather[0].icon
-    );
-    $(".day5Icon").text(
-      "Day 5 " + responseFiveDay.list[36].weather[0].icon
-    );
+    $(".day1icon").text("Day 1 " + responseFiveDay.list[4].weather[0].icon);
+    $(".day2icon").text("Day 2 " + responseFiveDay.list[12].weather[0].icon);
+    $(".day3icon").text("Day 3 " + responseFiveDay.list[20].weather[0].icon);
+    $(".day4icon").text("Day 4 " + responseFiveDay.list[28].weather[0].icon);
+    $(".day5icon").text("Day 5 " + responseFiveDay.list[36].weather[0].icon);
 
     $(".day1temp").text("Day 1 " + responseFiveDay.list[4].main.temp);
     $(".day2temp").text("Day 2 " + responseFiveDay.list[12].main.temp);
@@ -83,21 +69,11 @@ function getInput(cityName) {
     $(".day4temp").text("Day 4 " + responseFiveDay.list[28].main.temp);
     $(".day5temp").text("Day 5 " + responseFiveDay.list[36].main.temp);
 
-    $(".day1humidity").text(
-      "Day 1 " + responseFiveDay.list[4].main.humidity
-    );
-    $(".day2humidity").text(
-      "Day 2 " + responseFiveDay.list[12].main.humidity
-    );
-    $(".day3humidity").text(
-      "Day 3 " + responseFiveDay.list[20].main.humidity
-    );
-    $(".day4humidity").text(
-      "Day 4 " + responseFiveDay.list[28].main.humidity
-    );
-    $(".day5humidity").text(
-      "Day 5 " + responseFiveDay.list[36].main.humidity
-    );
+    $(".day1humidity").text("Day 1 " + responseFiveDay.list[4].main.humidity);
+    $(".day2humidity").text("Day 2 " + responseFiveDay.list[12].main.humidity);
+    $(".day3humidity").text("Day 3 " + responseFiveDay.list[20].main.humidity);
+    $(".day4humidity").text("Day 4 " + responseFiveDay.list[28].main.humidity);
+    $(".day5humidity").text("Day 5 " + responseFiveDay.list[36].main.humidity);
   });
 
   // local storage goes here
@@ -119,11 +95,3 @@ function getInput(cityName) {
   //   .css("textTransform", "capitalize")
   //   .appendTo(navItemOuterEl);
 }
-
-
-
-function example(x, y, z) {
-  console.log(x + y + z);
-}
-
-example(2, 3, 4);
